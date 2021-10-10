2f233137c53a250f201308b540e603f849f19469
Joaquín Fernández <67400456+Joako-Sador@users.noreply.github.com>
2021-10-10 02:29:44 -0300

    new commit

--- lechuga.py
+++ lechuga.py
@@ -33,8 +33,16 @@ def BintoHex(a):
     hstr = '%0*X' % ((len(a) + 3) // 4, int(a, 2))
     return hstr
 
+def hexToBinary(a,n):
+    scale = 16 ## equals to hexadecimal
+    num_of_bits = 8
+    newBinnary = ''
+    #print(bin(int(a, scale))[2:].zfill(num_of_bits))
+    newBinnary = bin(int(a, scale))[2:].zfill(num_of_bits)
+    return (('0'*n)+newBinnary)
+
 if __name__ == "__main__":
-    word = "tres tigres en un trigal aa"
+    word = "tres tigres en un trigal AÁ"
     newWord = ""
     cont = 0
     Tehasheo = ''
@@ -47,7 +55,8 @@ if __name__ == "__main__":
     else:
         #len(word) >= 25
         intervalo = len(word) / 25
-        arr = []; arr1 = []; values2hexad =[]; hexad_clean = []; binary_matrix = []
+        arr = []; arr1 = []; values2hexad = []; hexad_clean = []; binary_matrix = []
+
         for i in range(25):
             #print("[",int(intervalo*i),",",int(intervalo*(i+1)),"[","==",str(word[int(intervalo*i):int(intervalo*(i+1))]))
             arr.append(word[int(intervalo*i):int(intervalo*(i+1))])
@@ -94,18 +103,29 @@ if __name__ == "__main__":
         print(BintoHex(longitud_64bits))
         mensaje_bin_1_xceros = new_mensaje + longitud_64bits
         print(len(mensaje_bin_1_xceros))
-
-
-
-
-
-
-
         
+        h0 = hexToBinary('0x6a09e667'.replace('0x', ''),8)
+        h1 = hexToBinary('0xbb67ae85'.replace('0x', ''),8)
+        h2 = hexToBinary('0x3c6ef372'.replace('0x', ''),8)
+        h3 = hexToBinary('0xa54ff53a'.replace('0x', ''),8)
+        h4 = hexToBinary('0x510e527f'.replace('0x', ''),8)
+        h5 = hexToBinary('0x9b05688c'.replace('0x', ''),8)
+        h6 = hexToBinary('0x1f83d9ab'.replace('0x', ''),8)
+        h7 = hexToBinary('0x5be0cd19'.replace('0x', ''),8)
+        
+        print(h0)
+        print(h1)
+        print(h2)
+        print(h3)
+        print(h4)
+        print(h5)
+        print(h6)
+        print(h7)
+
         suma = ''
         for i in range(len(transpose_matrix)):
             for j in transpose_matrix[i]:
                 suma += (solver[iterator]+j+solver[iterator-1]+solver[iterator-2]+solver[iterator-3]+solver[iterator-4]+solver[iterator-5])
                 iterator=iterator+1
         
-        #print(Compactar(suma))
+        print(Compactar(suma))
