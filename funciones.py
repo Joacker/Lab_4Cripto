8b439ca4bf037ea9355bcc98c33cebaac7e2cbf9
Joaquín Fernández <67400456+Joako-Sador@users.noreply.github.com>
2021-10-14 22:36:16 -0300

    new hash

--- funciones.py
+++ funciones.py
@@ -5,101 +5,6 @@ import string, numpy, time, random, sys, math
 solver = [];
 
 #Funciones
-def myHash(hexad_clean, word):
-    binary_matrix = []
-    scale = 16; binary_line = []; iterator = 0
-    for i in range(len(hexad_clean)):
-        hexad_clean[i] = bin(int(hexad_clean[i], scale))[2:].zfill(8)
-        binary_matrix.append([hexad_clean[i]])
-    #print(binary_matrix)
-    suma1 = ''; mensajebin = []
-    transpose_matrix = numpy.transpose(binary_matrix)
-    for i in range(len(transpose_matrix)):
-        for j in transpose_matrix[i]:
-            suma1 += j
-    suma1 = suma1+'1'
-    #print(suma1)
-    module = ((448-1-len(mensajebin))%512)
-    #print(module)
-    zeros = '0'*module 
-    new_mensaje = suma1 + zeros
-    # print(new_mensaje)
-    for i in new_mensaje:
-        mensajebin.append(i)
-    # print(mensajebin)
-    longitud_hex = hex(len(word)*8)
-    #print(longitud_hex)
-    longitud_64bits = ""
-    longitud_hex=longitud_hex.replace('0x', '')
-    for i in toBinary(longitud_hex):
-        longitud_64bits = longitud_64bits + str(i)    
-
-    longitud_64bits = ('0'*64)+longitud_64bits
-    #print(longitud_64bits)
-    #print(BintoHex(longitud_64bits))
-    mensaje_bin_1_xceros = new_mensaje + longitud_64bits
-    # print(len(mensaje_bin_1_xceros))
-    
-    h0 = hexToBinary('0x6a09e667'.replace('0x', ''),8)
-    h1 = hexToBinary('0xbb67ae85'.replace('0x', ''),8)
-    h2 = hexToBinary('0x3c6ef372'.replace('0x', ''),8)
-    h3 = hexToBinary('0xa54ff53a'.replace('0x', ''),8)
-    h4 = hexToBinary('0x510e527f'.replace('0x', ''),8)
-    h5 = hexToBinary('0x9b05688c'.replace('0x', ''),8)
-    h6 = hexToBinary('0x1f83d9ab'.replace('0x', ''),8)
-    h7 = hexToBinary('0x5be0cd19'.replace('0x', ''),8)
-
-
-    ht = h0+h1+h2+h3+h4+h5+h6+h7
-    
-    otherword = ''
-    #print(otherword)
-    otherword ='♫☼►◄↕‼¶§▬↨↑↓→∟↔▲▼123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~⌂¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ'
-    mensaje_bin_1_xceros+='0'*298
-    newmessage = otherword+BintoHex(mensaje_bin_1_xceros)+BintoHex(ht)+otherword
-    #print(newmessage)
-    for i in solver:
-        newmessage = newmessage + i
-    #print(newmessage)
-    
-    valor = (Compactar(passGenerator(newmessage)))
-    return(valor)
-
-def newPass(pwd):
-    hexad_clean = []; values2hexad = []; arr = []; arr1 = [];
-    if (len(pwd)<25):
-        suma = 0
-        cantidad = 25 - len(pwd)
-        abecedario = string.ascii_letters + string.digits
-        word = pwd+''.join(random.choice(abecedario) for j in range(0,cantidad))
-        for i in range(25):
-            for j in word:
-                suma = suma + ord(j)
-            arr1.append(suma)
-            values2hexad.append(hex(arr1[i]))
-            for j in values2hexad[i::2]:
-                j = j.replace('0x', '')
-                hexad_clean.append(j)
-                solver.append(j)
-
-        return (hexad_clean)
-
-    intervalo = len(pwd) / 25
-    for i in range(25):
-        #print("[",int(intervalo*i),",",int(intervalo*(i+1)),"[","==",str(word[int(intervalo*i):int(intervalo*(i+1))]))
-        #print(word[int(intervalo*i):int(intervalo*(i+1))])
-        arr.append(pwd[int(intervalo*i):int(intervalo*(i+1))])
-        suma=0
-        for j in arr[i]:
-            suma = suma + ord(j)
-        arr1.append(suma)
-        values2hexad.append(hex(arr1[i]))
-        for j in values2hexad[i::2]:
-            j = j.replace('0x', '')
-            hexad_clean.append(j)
-            solver.append(j)
-    #print(hexad_clean)
-    return (hexad_clean)
 
 def Compactar(password):    
     while len(password) > 25:
@@ -124,14 +29,13 @@ def BintoHex(a):
     hstr = '%0*X' % ((len(a) + 3) // 4, int(a, 2))
     return hstr
 
-def hexToBinary(a,n):
+def hexToBinary(a):
     scale = 16 ## equals to hexadecimal
     num_of_bits = 8
     newBinnary = ''
     #print(bin(int(a, scale))[2:].zfill(num_of_bits))
     newBinnary = bin(int(a, scale))[2:].zfill(num_of_bits)
-    return (('0'*n)+newBinnary)
+    return (newBinnary)
 
-def passGenerator(pwd):
-    cantidad = len(pwd)
-    return(''.join(random.choice(pwd) for j in range(0,cantidad)))
+def Entropia(pwd):
+    pass
--- lechuga.py
+++ lechuga.py
@@ -1,41 +1,74 @@
 #Imports
-import sys, time
-from AVL import AVLTree
-from funciones import newPass, myHash
-
+import string
+import sys, time, os
+from funciones import Compactar,hexToBinary
+from datetime import datetime, timedelta
+futuredate = datetime.now() + timedelta(days=10)
 #Variables de entorno
 size = 0
+diccionario = string.ascii_letters+string.digits+'♫☼►◄↕‼¶§▬↨↑↓→∟↔▲▼123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~⌂¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ'
 
-#Main   
+def name(n):
+     return (n**2)+2*n+1
+
+tiempo = time.time()
+valores = 0
+seconds = futuredate.toordinal()
+
+def Segmentation25(pwd):
+    arr = []; base = 0; solver = []; largos = []
+    intervalo = len(pwd) / 25
+    dicIntervalo = len(diccionario) / 25
+    for i in range(25):
+        #print("[",int(intervalo*i),",",int(intervalo*(i+1)),"[","==",str(word[int(intervalo*i):int(intervalo*(i+1))]))
+        #print(word[int(intervalo*i):int(intervalo*(i+1))])
+        
+        k = len((pwd[int(intervalo*i):int(intervalo*(i+1))]))
+
+        arr.append(((pwd[int(intervalo*i):int(intervalo*(i+1))])))
+        
+        largos.append((len(arr[i]))*k*(i+1))
+        #c = (x + n) % 26   
+        suma = 0
+        
+        for j in range(len(arr[i])):
+            suma +=  ((valores*int(seconds))) + int(((name((ord(arr[i][j])**11)*i*j*seconds)) ** 0.3*valores*seconds*j) * ((i*(valores*i)*seconds+i)+2*(i+1)) * 11)*(largos[-1]*valores*j)
+            #print(suma)
+            if (j == (len(arr[i]) - 1)):
+                solver.append(suma*(seconds*j-1))    
+        
+    return solver,largos
+
+def newHash(palabra):
+    if (len(palabra)<25):
+        #palabra = palabra + '0'*(25-len(palabra))
+        sumita = 0
+        for i in palabra:
+            sumita = ord(i)
+        for i in range(25):
+            palabra = str(seconds)+palabra+i*str(seconds)+ diccionario[(i*sumita+int(seconds**i))%len(diccionario)]
+
+    largos = Segmentation25(palabra)[1]
+    array = Segmentation25(palabra)[0]
+    array.sort()
+    #print(array)
+    cont = 1
+    myhash=''; vector = []
+    for i in range(len(array)):
+        myhash += diccionario[(array[i]*largos[i-1]*(i+1))%len(diccionario)]
+        #vector.append(chr(Segmentation25(myhash)[0][i])+str(Segmentation25(chr(array[i]).encode('utf-8', 'replace').decode())[0][i])*i)
+        cont+=1
+    
+    return(Compactar(myhash))
+
 if __name__ == "__main__":
-    seguir = True; Tree = AVLTree(); root = None
-    size+=1
-    root = Tree.insert(root,size,'root','root')
-    decision = ''
-    while(seguir):
-        #root = None
-        word = input('Ingrese password: ')
-        word = str(word)
-        hexad_clean = newPass(word)
-        #print(myHash(hexad_clean,word))
-        if (Tree.getHeight(root)==1):
-            hashedpass = myHash(hexad_clean,word)
-            size+=1
-            root = Tree.insert(root, size, hashedpass, word)
-            print(hashedpass)  
-        else:
-            if(Tree.Search(root,word)):
-                print('entrando a busqueda')
-                print(Tree.Search(root,word)[1])
-            else:
-                print('agregando elementos')
-                hashedpass = myHash(hexad_clean,word)
-                size+=1
-                root = Tree.insert(root, size, hashedpass, word)
-                print(hashedpass)
-        #root = Tree.insert(root, size, hashedpass, word)
-        decision = input('Desea continuar ¿Si o No?\n')
-        if (decision == 'No'):
-            #print(Tree.preOrder(root))
-            seguir = False
-        word = ''
+    #print(sys.argv)
+    palabra = ' '
+    word = palabra.encode("utf-8").hex()
+    kPonder = 0
+    for i in (hexToBinary(word)):
+        kPonder+=(int(i)*(len(hexToBinary(word))//3))
+    valores = kPonder
+    print(newHash(palabra))
+    #print(kPonder)
+    
